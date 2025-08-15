4b553e2dab665ea09795ce50b5fc65ef0018968c
China Hagström <chinahg@mit.edu>
2025-08-15 14:57:35 -0400

    Added CoCiP/APCEMM/LES comparisons. Incomplete.

---
+++ APCEMM_vs_CoCiP_vs_LES/APCEMM/run_apcemm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                   
+
+#SBATCH --time=96:00:00
+#SBATCH --constraint=tengig
+#SBATCH --job-name="2.5 Lapse Full LES APCEMM run"
+#SBATCH --mail-type=BEGIN,END
+#SBATCH -o /home/chinahg/GCresearch/contrailuncertainty/APCEMM_vs_CoCiP_vs_LES/APCEMM/slurm-%j-out
+#xSBATCH -e slurm-%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=normal
+#SBATCH --mem-per-cpu=20000MB
+
+#####################################
+
+cd /home/chinahg/GCresearch/APCEMM/examples/issl_rhi140
+pwd
+/home/chinahg/GCresearch/APCEMM/build/APCEMM /home/chinahg/GCresearch/contrailuncertainty/APCEMM_vs_CoCiP_vs_LES/APCEMM/$ARG1/B767_LES_CoCiP_APCEMM_input.yaml
